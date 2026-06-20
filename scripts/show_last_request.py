3aac9467068de21c2c1cd2fb8b593f4646c6e3af
stevef1uk <stevef1uk@gmail.com>
2026-06-20 16:10:57 +0200

    tweak to scripts files

--- scripts/show_last_request.py
+++ scripts/show_last_request.py
@@ -1,5 +1,5 @@
-import json
-d=json.load(open('/home/stevef/dev/freeride/last_payload.json'))
+import json, os
+d=json.load(open(os.path.join(os.path.expanduser('~'), 'dev/freeride/last_payload.json')))
 for i,m in enumerate(d['messages']):
     print('===', i, m['role'], 'len', len(m['content']), '===')
     print(m['content'])
